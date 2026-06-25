b3ad24168ab9f72eedfa83a6267c3b39f5436f87
jesusraian444-a11y <jesus.raian.444@gmail.com>
2026-06-25 15:33:24 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("⚽ Mi Analizador de Fútbol")
+st.write("¡Bienvenido a mi app!")
+
+if st.button('Analizar partidos'):
+    st.write("El motor está funcionando correctamente.")
